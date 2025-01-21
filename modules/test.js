0f4e46b7eff7e2af3cd9047c01a753ceb281660a
initiator <3168194067@qq.com>
2025-01-21 21:39:19 +0800

    update 2025-01-21T21:39:18.031787

--- modules/test.js
+++ modules/test.js
@@ -1,6 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.test = test;
-function test() {
+export function test() {
     return "this is a test function return a test string";
 }
